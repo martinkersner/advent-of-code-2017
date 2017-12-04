f73bcebb7f117136e5895d069e2fd11822918ec7
Martin Kersner <m.kersner@gmail.com>
2017-12-04 22:33:08 +0900

    second part of 4th day

---
+++ 4-2.py
@@ -0,0 +1,19 @@
+import sys
+import hashlib
+
+def hash(string):
+    string = "".join(sorted(string))
+    b = bytearray()
+    b.extend(string.encode())
+    return hashlib.sha224(b).hexdigest()
+
+
+valid = 0
+with open(sys.argv[1], "r") as f:
+    for line in f:
+        input = line.strip().split()
+        output = set([hash(s) for s in input])
+        if len(input) == len(output):
+            valid += 1
+
+print(valid)
